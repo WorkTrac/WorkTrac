dd115f1424a2593cee8a2d948e400c8b1bf1593d
WorkTrac <worktrac.skw@gmail.com>
2026-08-20 13:51:32 +0530

    Update app.js
    added cookie parser

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 // redeploy
 const express = require("express");
 const cors = require("cors");
+const cookieParser = require("cookie-parser"); 
 
 const attendanceRoutes = require("./routes/attendanceRoutes");
 const adminRoutes = require("./routes/adminRoutes");
@@ -28,7 +29,7 @@ app.use(cors({
   origin: allowedOrigins,
   credentials: true,
 }));
-
+app.use(cookieParser());
 app.use(express.json());
 
 // ===============================
